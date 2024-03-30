6b0a447fc02aafdab3bd86210c4dd1bc3a806896
OM SHUKLA <89527063+shuklaaum@users.noreply.github.com>
2024-03-31 00:56:40 +0530

    Update Home.py
    I just added some content to streamlit.write section.

--- Home.py
+++ Home.py
@@ -1,60 +1,51 @@
 import streamlit as st
 import pandas
 
-# Set webpage layout to wide
+
 st.set_page_config(layout="wide")
 
-# Add a header and some other text
+
 st.header("The Best Company")
 st.write("""
-Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
-tempor incididunt ut labore et dolore magna aliqua. 
-Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
-aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
-in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
-officia deserunt mollit anim id est laborum.""")
+Welcome to The Best Company, where innovation meets excellence. We pride ourselves on our dedication to delivering top-notch solutions that exceed expectations. Our team of experts works tirelessly to ensure every project we undertake is a resounding success. Explore our website to learn more about our services and how we can help you achieve your goals.
+""")
 st.subheader("Our Team")
 
-# Prepare the columns
+
 col1, col2, col3 = st.columns(3)
 
-# Make a dataframe with the company members
+
 df = pandas.read_csv("data.csv")
 
-# Add content to the first column
+
 with col1:
-    # Iterate over only the first four rows
+    
     for index, row in df[:4].iterrows():
-        # Add member's first and last names
+        
         st.subheader(f'{row["first name"].title()} {row["last name"].title()}')
-        # Add member's role in the company
+        
         st.write(row["role"])
-        # Add member's photo
+        
         st.image("images/" + row["image"])
 
-# Add content to the second column
+
 with col2:
-    # Iterate over rows 4 to 7
+   
     for index, row in df[4:8].iterrows():
-        # Add member's first and last names
+        
         st.subheader(f'{row["first name"].title()} {row["last name"].title()}')
-        # Add member's role in the company
+      
         st.write(row["role"])
-        # Add member's photo
+       
         st.image("images/" + row["image"])
 
 
 with col3:
-    # Iterate over rows 4 to 7
+    
     for index, row in df[8:].iterrows():
-        # Add member's first and last names
+        
         st.subheader(f'{row["first name"].title()} {row["last name"].title()}')
-        # Add member's role in the company
+       
         st.write(row["role"])
-        # Add member's photo
+      
         st.image("images/" + row["image"])
-
-
-
-
